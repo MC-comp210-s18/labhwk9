d618db4b7cf4f726ead1dbc160b2a0e318a722cb
Robert Utterback <robertu13@gmail.com>
2018-03-29 10:41:02 -0500

    Added more explanation

--- src/tempconvert/DesktopController.java
+++ src/tempconvert/DesktopController.java
@@ -2,7 +2,16 @@ package tempconvert;
 
 public class DesktopController implements IConverterController {
 
-	// Set the right action for the convert button, and add the view to the model's observers.
+  // In Java 8 and with Eclipse 4+ (we have 3.8) making the event
+  // handler here is easy. In our case, there are two ways of doing
+  // this with an anonymous inner class:
+  // (1) make the model and view private fields of this class,
+  // DesktopController, and use them in the anonymous inner class.
+  // (2) make the model and view private fields of the anonymous inner
+  // class itself.
+
+	// Set the right action for the convert button, and add the view to
+	// the model's observers.
 	@Override
 	public void connect(IConverterModel m, IConverterView v) {
 		// @TODO
